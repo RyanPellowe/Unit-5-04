2caaf81557feb6081f0236a5778ec2c83109307c
RyanPellowe <ryan.pellowe@ocsbstudent.ca>
2021-02-12 14:44:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("report").addEventListener("click", reportresults)
+
+function reportresults () {
+  alert(document.getElementById("input").value)
+}
+
+document.getElementById("paragraph").innerHTML = "Give that textbox a try!";
+
+document.getElementById("color").addEventListener("click", buttonOn)
+
+function 
